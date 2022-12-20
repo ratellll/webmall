83b3e27939d6df3d7d46d92d1fba9aac2e1e7325
ratellll <latellbin@naver.com>
2022-12-20 16:07:58 +0900

    쇼핑몰다시

---
+++ src/main/java/com/example/webmall/WebmallApplication.java
@@ -0,0 +1,13 @@
+package com.example.webmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webmall/WebmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
